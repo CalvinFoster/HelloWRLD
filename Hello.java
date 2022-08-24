2d756d9c346ca9bee1c1ed9d1682eddd19558cc8
CalvinFoster <112003670+CalvinFoster@users.noreply.github.com>
2022-08-24 12:17:24 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/********************************************************************************************
+* Java JDK
+* Calvin Foster
+* August 24, 2022
+* CMSC255 004
+********************************************************************************************/
+
+public class Hello{
+	public static void main(String[] args) {
+
+		System.out.println("<Calvin Foster>");
+		System.out.println("<Java JDK>");
+		System.out.println("<Computer Science>");
+		System.out.println("<August 24, 2022>");
+		System.out.println("<CMSC255 004>");
+		System.out.println();
+	}
+}
